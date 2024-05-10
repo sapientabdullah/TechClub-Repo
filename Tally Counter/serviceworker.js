f892d0dd658105d8e798864214e864073d9ae682
Abdullah <sapientabdullah@gmail.com>
2024-05-10 06:33:37 +0500

    PWA

---
+++ Tally Counter/serviceworker.js
@@ -0,0 +1,23 @@
+const assets = ["/", "tallyCounter.css", "tallyCounter.js", "sw-register.js"];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open("assets").then((cache) => {
+      cache.addAll(assets);
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      const fetchPromise = fetch(event.request).then((networkResponse) => {
+        caches.open("assets").then((cache) => {
+          cache.put(event.request, networkResponse.clone());
+          return networkResponse;
+        });
+      });
+      return response || fetchPromise;
+    })
+  );
+});
